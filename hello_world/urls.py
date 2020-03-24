e9424feb08251a5b81aaae98b51330508329ff55
Marissa Kachadoorian <mdkach@bu.edu>
2020-03-23 22:55:24 -0400

    Initialcommit

---
+++ hello_world/urls.py
@@ -0,0 +1,10 @@
+#file: hello_world/urls.py
+#description: direct URL requests to view functions
+
+from django.urls import path
+from .views import homePageView
+
+urlpatterns = [
+    path('', homePageView, name='home'),
+
+]
