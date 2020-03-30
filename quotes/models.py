4f7bd283a55d1993ac567539f01f9825e3a75c9b
Marissa Kachadoorian <mdkach@bu.edu>
2020-03-30 17:09:09 -0400

    examples for assignment 16

--- quotes/models.py
+++ quotes/models.py
@@ -1,16 +1,61 @@
 from django.db import models
-
+import random
 # Create your models here.
+class Person(models.Model):
+    """Encapsulate the concept of a person, who said some famous quote."""
+
+    name = models.TextField(blank=False)
+
+    def __str__(self):
+        """Return a string representation of this Person."""
+        return self.name
+
+    #get an image at random
+    def get_random_image(self):
+        """Return an image of this person, chosen at random."""
+
+        #get all images of this person
+        images = Image.objects.filter(person=self.pk)
+
+        #then, pick one at random, and return it
+        i = random.randint(0, len(images) -1)
+        return images[i]
+
+    #get all images for this person
+
+    def get_all_images(self):
+        """return a QuerySet of all images for this person."""
+        
+        #get all images of this person
+        images = Image.objects.filter(person=self.pk)
+        return images
+
+    #get all quotes for this person
+
+    def get_all_quotes(self):
+        """return a QuerySet of all quotes for this person."""
+        
+        #get all images of this person
+        quotes = Quote.objects.filter(person=self.pk)
+        return quotes
 
 class Quote(models.Model):
     """Encapsulate the idea of a quote (i.e. text)."""
 
     #data attributes of a quote:
     text = models.TextField(blank=True)
-    author = models.TextField(blank=True)
-    image_url = models.URLField(blank=True)
-
-    def __repr__(self):
+    person = models.ForeignKey('Person', on_delete="CASCADE")
+    
+    def __str__(self):
         """Return a string representation of this subject."""
-        return '"%s" - %s' % (self.text, self.author)
+        return '"%s" - %s' % (self.text, self.person.name)
 
+class Image(models.Model):
+    """Represent an image, which is associated with a person."""
+
+    image_url = models.URLField(blank=True)
+    person = models.ForeignKey('Person', on_delete="CASCADE")
+
+    def __str__(self):
+        """Return a string representation of this image."""
+        return self.image_url
--- quotes/urls.py
+++ quotes/urls.py
@@ -3,11 +3,12 @@
 #description: direct URL requests to view functions
 
 from django.urls import path
-from .views import HomePageView, QuotePageView, RandomQuoteView
+from .views import * #HomePageView, QuotePageView, RandomQuoteView, PersonPageView
 
 urlpatterns = [
     path('', RandomQuoteView.as_view(), name="random"), #pick a random quote
-    path('all', HomePageView.as_view(), name='home'),
-    path('quote/<int:pk>', QuotePageView.as_view(), name='quote'),
+    path('all', HomePageView.as_view(), name='home'), #generic class based view
+    path('quote/<int:pk>', QuotePageView.as_view(), name='quote'), #show one quote
+    path('person/<int:pk>', PersonPageView.as_view(), name='person'), #show one person
 
 ]
